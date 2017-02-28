f140b20a959de7a7d916a49280ea85c523328135
Elke Fasshauer <elke.fasshauer@gmail.com>
2017-03-01 00:29:00 +0100

    function create_normal_order works

--- deltas.py
+++ deltas.py
@@ -13,18 +13,29 @@
 # written by:  Elke Fasshauer February 2017                      #
 ##################################################################
 
-def is_legitimate(operators):
-   "Determine whether the number of creation and annihilation operators match"
+def number_create(operators):
    n = len(operators)
    ncreate = 0
-   nannihilate = 0
    for pos in range(0,n):
       operator_character = operators[pos][1]
       if operator_character == 'create':
          ncreate = ncreate + 1
+   return ncreate
+
+def number_annihilate(operators):
+   n = len(operators)
+   nannihilate = 0
+   for pos in range(0,n):
+      operator_character = operators[pos][1]
       if operator_character == 'annihilate':
          nannihilate = nannihilate + 1
-   
+   return nannihilate
+
+def is_legitimate(operators):
+   "Determine whether the number of creation and annihilation operators match"
+   n = len(operators)
+   ncreate = number_create(operators)
+   nannihilate = number_annihilate(operators)
    if ncreate == nannihilate:
       legitimate = True
    else:
@@ -72,19 +83,54 @@ def find_pos_to_change(operators):
       pos_ann = first_ann
    return (pos_ann,pos_create)
    
-def find_indizes_to_change(operators,positions):
+def find_indices_to_change(operators,positions):
    (pos1,pos2) = positions
    i1 = operators[pos1][0]
    i2 = operators[pos2][0]
    return (i1,i2)
 
+def create_normal_order(op_strings):
+   "operates on a list of operator strings"
+   ncreate = number_create(op_strings[0])
+   maxperm = ncreate**4
+   for perm in range(0,maxperm):
+      n_strings = len(op_strings)
+      for i in range(0,n_strings):
+         curr_string = op_strings[i]
+         if (is_legitimate(curr_string) == False):
+            print 'Non-legitimate string'
+            break
 
+         if (is_normal_ordered(curr_string) == False):
+            positions = find_pos_to_change(curr_string)
+            indices = find_indices_to_change(curr_string,positions)
+            new_string = curr_string[:]
+            (i1,i2) = indices
+            (pos1,pos2) = positions
+            # Delta function
+            del curr_string[pos2]
+            del curr_string[pos1]
+            curr_string.append(('Delta',i1,i2))
+            #curr_string.append((i1,i2))
+            # swap indices
+            new_string[pos1], new_string[pos2] = new_string[pos2], new_string[pos1]
+            new_string.append(('-','-'))
+            op_strings.append(new_string)
+   print op_strings
+
+all_strings = []
 op_string = [('a', 'create'), ('i', 'annihilate'), ('b', 'create'), ('j', 'annihilate')]
+all_strings.append(op_string)
 
-a = is_normal_ordered(op_string)
-#print a
-#find_pos_to_change(op_string)
-positions = find_pos_to_change(op_string)
-indizes = find_indizes_to_change(op_string,positions)
-print positions
-print indizes
+create_normal_order(all_strings)
+      
+#print all_strings
+   
+
+#a = is_normal_ordered(op_string)
+##print a
+##find_pos_to_change(op_string)
+#positions = find_pos_to_change(op_string)
+#indices = find_indices_to_change(op_string,positions)
+#print positions
+#print indices
